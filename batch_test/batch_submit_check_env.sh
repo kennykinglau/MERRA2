69b6d688cf9a82d0e4f4fe1bda094edfb09015ee
Neil Francis Goeckner-Wald <ngoeckne@sh02-ln03.stanford.edu>
2021-05-05 15:44:44 -0700

    batch submission doesnt know about python being aliased to python3

--- batch_test/batch_submit_check_env.sh
+++ batch_test/batch_submit_check_env.sh
@@ -1,11 +1,11 @@
 #! /usr/bin/env bash
 #
-#SBATCH --job-name=SPconsistency_job
+#SBATCH --job-name=env_test
 #
 #SBATCH --time=00:03:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=4G
+#SBATCH --mem-per-cpu=2G
 
-env
+python3 -c "import matplotlib; import numpy; import scipy; import netCDF4; import pandas"
 
